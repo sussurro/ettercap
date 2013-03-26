ee7015b8b1bd9e2b0b7046c493b0b0793f007015
root <root@bt.foo.org>
2013-03-25 21:24:25 -0400

    initial update to add sample lua hooks

---
+++ share/lua/scripts/http_creds.lua
@@ -0,0 +1,65 @@
+---
+--
+-- Created by Ryan Linn and Mike Ryan
+-- Copyright (C) 2012 Trustwave Holdings, Inc.
+
+description = "Script to show HTTP requsts";
+
+local http = require("http")
+local packet = require("packet")
+
+hook_point = http.hook 
+
+packetrule = function(packet_object)
+
+   -- If this isn't a tcp packet, it's not really a HTTP request
+   -- since we're hooked in the HTTP dissector, we can assume that this
+   -- should never fail, but it's a good sanity check
+  if packet.is_tcp(packet_object) == false then
+    return false
+  end
+
+  return true
+end
+
+
+
+-- Here's your action.
+action = function(packet_object) 
+   local p = packet_object
+   
+   -- Parse the http data into an HTTP object
+   local hobj = http.parse_http(p)
+
+   -- If there's no http object, get out
+   if hobj == nil then
+      return
+   end
+
+   -- Get out session key for tracking req->reply pairs
+   local session_id = http.session_id(p,hobj)
+
+   -- If we can't track sessions, this won't work, get out
+   if session_id == nil then
+      return
+   end
+
+   -- We have a session, lets get our registry space
+   local reg = ettercap.reg.create_namespace(session_id)
+
+   -- If it's a request, save the request to the registry
+   -- We'll need this for the response
+   if hobj.request then 
+      if hobj.creds then
+       
+         -- Log the request/response with the redirect
+         ettercap.log("HTTP_CREDS: %s:%d -> %s:%d %s %s [User:Pass = %s]\n",
+            packet.src_ip(p),
+            packet.src_port(p), 
+            packet.dst_ip(p), 
+            packet.dst_port(p), 
+            hobj.verb ,hobj.url, hobj.creds)
+      end
+
+   end
+end
